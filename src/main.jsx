bffa35d3bd4baccb3904daed117dd878b0f9171d
Thiago-Dumont <ciamesp.bh@gmail.com>
2026-04-03 16:35:26 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Mudamos de DataPath para App para evitar erro de referência
-import App from './App.jsx' 
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
