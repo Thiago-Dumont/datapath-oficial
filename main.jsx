03cd5526233fc7e2ce605f33bab26d8c58ce16b9
Thiago-Dumont <ciamesp.bh@gmail.com>
2026-04-03 11:58:16 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import DataPath from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataPath />
   </React.StrictMode>,
 )
